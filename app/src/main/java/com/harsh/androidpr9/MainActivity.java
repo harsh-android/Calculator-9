24151d18cd7447c4e37d463c48cc8b643c8333cd
milanbhut <milanbhut7199@gmail.com>
2023-02-13 09:56:10 +0530

    Commit 2

--- app/src/main/java/com/harsh/androidpr9/MainActivity.java
+++ app/src/main/java/com/harsh/androidpr9/MainActivity.java
@@ -12,7 +12,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView n1,n2,n3,n4,n5,n6,n7,n8,n9,n00,n0,input;
+    TextView n1,n2,n3,n4,n5,n6,n7,n8,n9,n00,n0,input,plus,ans,ac;
+
+    int num1,num2,sign;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -32,95 +34,128 @@ public class MainActivity extends AppCompatActivity {
         n9 = findViewById(R.id.n9);
         n0 = findViewById(R.id.n0);
         n00 = findViewById(R.id.n00);
+        plus = findViewById(R.id.plus);
+        ans = findViewById(R.id.ans);
+        ac = findViewById(R.id.ac);
+
+        ac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText("");
+                num1 = num2 = 0;
+            }
+        });
 
         n1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num = input.getText().toString();
-                input.setText(num+"1");
+                setNumber("1");
 
             }
         });
         n2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num = input.getText().toString();
-                input.setText(num+"2");
+                setNumber("2");
 
             }
         });
         n3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num = input.getText().toString();
-                input.setText(num+"3");
+                setNumber("3");
 
             }
         });
         n4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num = input.getText().toString();
-                input.setText(num+"4");
+                setNumber("4");
 
             }
         });
         n5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num = input.getText().toString();
-                input.setText(num+"5");
+                setNumber("5");
 
             }
         });
         n6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num = input.getText().toString();
-                input.setText(num+"6");
+                setNumber("6");
 
             }
         });
         n7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num = input.getText().toString();
-                input.setText(num+"7");
+                setNumber("7");
 
             }
         });
         n8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num = input.getText().toString();
-                input.setText(num+"8");
+                setNumber("8");
 
             }
         });
         n9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num = input.getText().toString();
-                input.setText(num+"9");
+                setNumber("9");
 
             }
         });
         n0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num = input.getText().toString();
-                input.setText(num+"0");
+                setNumber("0");
 
             }
         });
         n00.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num = input.getText().toString();
-                input.setText(num+"00");
-
+               setNumber("00");
             }
         });
 
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num = input.getText().toString();
+                num1 = Integer.parseInt(num);
+                input.setText("");
+                sign = 1;
+            }
+        });
+
+        ans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String num = input.getText().toString();
+                num2 = Integer.parseInt(num);
+
+                switch (sign) {
+                    case 1:
+                        input.setText(""+(num1+num2));
+                        break;
+                    case 2:
+                        input.setText(""+(num1-num2));
+                        break;
+                }
+
+            }
+        });
     }
+
+    void setNumber(String n) {
+        String num = input.getText().toString();
+        input.setText(num+n);
+    }
+
 }
